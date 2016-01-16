139d3dbaa25cd764f7676e0d5c467665e5281c90
Sophia <milanov@mpia.de>
2016-01-16 21:37:46 +0100

    Wilma's comments applied partly

--- GC_Orbit_class.py
+++ GC_Orbit_class.py
@@ -2,12 +2,14 @@ import numpy as np
 from scipy import interpolate
 from scipy import integrate as intg
 from scipy import optimize as opt
-
+from scipy import constants as cs
+from astropy import units as un
+import sys
 
 
 
 class GCorbit:
-	def __init__(self,R,rho,bhmass): #filename of object containing interpolation of density profile
+	def __init__(self,r_pc=None,rho_Msunpc3=None,bhmass_Msun=None,inputfilename=None,n=None): 
         """
         NAME:
            __init__
@@ -31,56 +33,141 @@ class GCorbit:
         #           sys.exit("Error in GCorbit.__init__(): Specify input file or r and rho.")
         #       else:
         #           proceed with your code...
-        #3. I would always write the units that you are using, to make sure, that there are no unit conversion problems due to a wrong input.
-		self.r=R
-		self.rho=rho
+
+		if inputfilename is not None:
+			data=np.loadtxt(inputfilename)		
+			self._r  = data[0,:] #[pc]
+			self._rho = data[1,:] #[M_sun/pc³]
+		elif r is None and rho is None:
+			sys.exit("Error in GCorbit.__init__(): Specify input file or r and rho.")
+		else:
 		self.s=interpolate.InterpolatedUnivariateSpline(np.log(self.r[:]),np.log(self.rho[:]))
-		pot_y=self.potential(r,density,low=0.003,high=104.64,x_i=None,w_i=None,full_integration=True)   #11. What units??? Write it in code as comment.
-        #^-- 3. The limits that you use here are just for your current GC. To make it more flexible, use min(r) and max(r).
-        #    4. Maybe you want to calculate the xi and wi already in __init__ as well and pass them to the potential function...
-		self.pot=interpolate.InterpolatedUnivariateSpline(self.r,pot_y)
-        #^-- 5. Did you test if it makes sense to interpolate the potential in log-space as well?
+		pot_y=self.potential(r,density,low=np.min(r),high=np.max(r),x_i=None,w_i=None,full_integration=True) #   #11. What units??? Write it in code as comment.
+		self._G=un.m**3/(un.kg*un.s**2).to.(un.pc**3/(un.solMass*un.s**2), cs.G) #[pc³/M_sun*s²]
+		self._bhmass=bhmass_Msun	#M_sun
+		self._n=80
+		self._x_i=np.polynomial.legendre.leggauss(self.n)[0]        #    4. Maybe you want to calculate the xi and wi already in __init__ as well and pass them to the potential function...
+		self._w_i=np.polynomial.legendre.leggauss(self.n)[1]
+
+
+		self.pot=interpolate.InterpolatedUnivariateSpline(self.r,pot_y) #^-- 5. Did you test if it makes sense to interpolate the potential in log-space as well?
 
     #35. I think you need a function def density(self,r) that calls the interpolation object self.s with np.log(r) and returns the result as np.exp(self.s(np.log(r))), or something like that
 
-	def potential(self,r,density,low=0.003,high=104.64,x_i=None,w_i=None,full_integration=False):   
-        #^-- 6. I would call this _potential_stars, as you also have a _potential_bh function.
+	def density(self,r):
+	"""
+	NAME:
+		density
+	PURPOSE:
+		returns density at distance r
+	INPUT:
+		self.s = interpolation of density profile
+	OUTPUT:
+		
+	HISTORY:
+
+	"""
+		return np.exp(self.s(np.log(r)))
+
+	def _potential_stars(self,r,density=None,low=np.min(r),high=np.max(r),x_i=None,w_i=None,full_integration=False):   
+	"""
+	NAME:
+		_potential_stars
+	PURPOSE:
+		calculates potential caused by stars in Globular cluster
+	INPUT:
+		R = array of radius coordinates in pc (default: None)
+	OUTPUT:
+		Potential 
+	HISTORY:
+		2016-01-15 - Written (Milanov, MPIA)
+	"""
         #    15. density is only needed in case of full_integration. Make it a keyword as well, density=None, and test inside the if condition if it is really set.
-		if True:    #7. Why "if True:"??? Do you mean "if full_integration is True:"?
-				sum1=np.zeros(n)    #9. What is n???
-   				sum2=np.zeros(n)
-				if isinstance(r,np.ndarray):
-        				return np.array([potential(rr,density,low=low,high=high,x_i=x_i,w_i=w_i) for rr in r])
+		if r<low or r>high:
+            		sys.exit("r is smaller or bigger than star boundaries")
+
+		if full_integration=True:
+			if density is None:
+				sys.exit("Error in GCorbit._potential_stars(): Specify density.")    
+			sum1=np.zeros(self.n)    
+   			sum2=np.zeros(self.n)
+			if isinstance(r,np.ndarray):
+        			return np.array([potential(rr,density=density,low=low,high=high,x_i=x_i,w_i=w_i) for rr in r])
     			else:
-        				if r<low or r>high:
-            					sys.exit("r is smaller or bigger than star boundaries") #13. I would put this error message outside of the "if full_integration" bracket, at the beginning of the function.
-        				x1=((r-low)/2)*x_i+(r+low)/2    #8. Better write "2." as float. Maybe it works anyway, but it avoids mistakes if integer input. Also everywhere else, where you actually need a float and not an integer.
-        				x2=((high-r)/2)*x_i+(high+r)/2
-        				for i in range(n):
-            					s1=density(x1[i])   #36. This should be self.density(...)
-            					s2=density(x2[i])
-            					sum1[i]=(w_i[i]*x1[i]**2*s1)
-            					sum2[i]=(w_i[i]*x2[i]*s2)
-        				sum_1=np.sum(sum1)
-        				sum_2=np.sum(sum2)
-        				return -4*np.pi*G*((r-low)/(2*r)*sum_1+(high-r)/2*sum_2)    #10. What is G? What units? You could specify it as self.G in __init__. Make sure it has the correct units.
+        			x1=((r-low)/2.)*self._x_i+(r+low)/2.    
+        			x2=((high-r)/2)*self._x_i+(high+r)/2
+        			for i in range(n):
+            				s1=self.density(x1[i])   
+            				s2=self.density(x2[i])
+            				sum1[i]=(self._w_i[i]*x1[i]**2*s1)
+            				sum2[i]=(self._w_i[i]*x2[i]*s2)
+        			sum_1=np.sum(sum1)
+        			sum_2=np.sum(sum2)
+        			return -4*np.pi*self._G*((r-low)/(2*r)*sum_1+(high-r)/2*sum_2)   
 		else:
-			return self.pot(r,density,low=0.003,high=104.64,x_i=None,w_i=None)
-            #^-- 12. I don't think the interpolation object pot does have low, high etc. as input.
+			return self.pot(r,density)
     
-	def potential_bh(self,r,bhmass):
-		return -G*bhmass/r
-        #14. I would set self._bhmass and self._G in the __init__function, where you also make sure that the units are correct. Then potential_bh only needs r as input.
+	def _potential_bh(self,r):
+	"""
+	NAME:
 
-#addiere sternen- und bh-potential vor interpolation?
-#^-- 16. Nein. Du koenntest noch eine function potential(self,r) machen, die nix anderes tut, als _potential_stars(r)+_potential_bh(r) zu addieren. Allerdings ist das dann die, die du immer von aussen aufrufen moechtest. 
+	PURPOSE:
 
-	def r_derivative(self,potential):   #17. Sorry, no idea, what you're doing in this function.
+	INPUT:
+
+	OUTPUT:
+
+	HISTORY:
+
+	"""
+		return -self._G*self._bhmass/r
+
+
+	def potential(self,r):
+	"""
+	NAME:
+
+	PURPOSE:
+		adds star and black hole potential
+	INPUT:
+
+	OUTPUT:
+
+	HISTORY:
+
+	"""
+		return _potential_stars(r)+_potential_bh(r)
+	
+	def _r_derivative(self):   #17. Sorry, no idea, what you're doing in this function.
+	"""
+	NAME:
+		
+	PURPOSE:
+		calculates derivative of interpolated potential
+	INPUT:
+
+	OUTPUT:
+
+	HISTORY:
+
+	"""
 		return self.potential.derivative
         #20. I think having this function is good, but it should do something else. It could have a keyword explicit_derivative=False. In the __init__ function you could call this function once with explicit_derivative=True and calculate the derivative of the potential explicitly at a few points. Everywhere else you use the interpolation analogous to the potential.
         #21. This function should have a parameter r at which you want to know the potential derivative.
 
-	def force(self,x,y,z,potential):    #18. I don't think, you have to pass potential to the force function. You can use inside force() the function self.potential(r).
+	def force(self,x,y,z):    
+	"""
+	NAME:
+
+	PURPOSE:
+
+	INPUT:
+
+	OUTPUT:
+
+	HISTORY:
+
+	"""
 		force=np.array(3)
 		r=np.sqrt(x**2+y**2+z**2)
 		drdx=x/r
@@ -92,7 +179,24 @@ class GCorbit:
 		return force
 		
 
-	def orbit(self,x0,y0,z0,vx0,vy0,vz0,N): #noch von force abh�ngig machen?    #22. Is this the orbit integration function? I would, in addition to the initial star position, have delta_t and t_end (in useful units) as parameters. N is then calculated in the function from that.
+	def orbit(self,x0,y0,z0,vx0,vy0,vz0,dt=None,t_end=None):     #22. Is this the orbit integration function? I would, in addition to the initial star position, have delta_t and t_end (in useful units) as parameters. N is then calculated in the function from that.
+	"""
+	NAME:
+
+	PURPOSE:
+
+	INPUT:
+
+	OUTPUT:
+
+	HISTORY:
+
+	"""	
+		
+		t_start=0
+		N=t_end/dt
+		t=np.linspace(t_start,t_end,N)
+
 		xl=np.zeros(N+1)
 		yl=np.zeros(N+1)
 		zl=np.zeros(N+1)
@@ -128,7 +232,7 @@ class GCorbit:
     			vyl[i+1]=vyl[i]+1./2.*(a[1]+a_1[1])*dt
    			vzl[i+1]=vzl[i]+1./2.*(a[2]+a_1[2])*dt
 		
-		return xl,yl,zl,vxl,vyl,vzl #23. You could also return the time array. Easier for plotting...
+		return xl,yl,zl,vxl,vyl,vzl,t 
 
     #^-- 33. Hab mir die Funktion nicht im Detail angeschaut. Gehe davon aus, dass es stimmt.
     #^-- 34. Da du Runge-Kutta und leapfrog implementiert hast, koenntest du ein keyword method='leapfrog' setzen, dass man auch zu 'rk4' setzen kann, wenn man moechte. Dann kannst du einfach hin und her switchen.
@@ -137,6 +241,18 @@ class GCorbit:
 ##### Versuch 1 zu actions #####
 
 	def angularmom(self,x,y,z,vx,vy,vz):
+	"""
+	NAME:
+
+	PURPOSE:
+
+	INPUT:
+
+	OUTPUT:
+
+	HISTORY:
+
+	"""
     		Lx=y*vz-z*vy
     		Ly=z*vx-x*vz
     		Lz=x*vy-y*vx
@@ -145,15 +261,39 @@ class GCorbit:
 
 
 	def energy(self,x,y,z,vx,vy,vz):
+	"""
+	NAME:
+
+	PURPOSE:
+
+	INPUT:
+
+	OUTPUT:
+
+	HISTORY:
+
+	"""
     		pot=self.potential(x,y,z)		#eventuell weitere Parameter im Potential #24. Noe, ich glaub nicht...
     		E=vx**2./2.+vy**2./2.+vz**2./2.+pot
     		return E
 
 	def periapocenter(self,r_ap,x,y,z,vx,vy,vz):
+	"""
+	NAME:
+
+	PURPOSE:
+
+	INPUT:
+
+	OUTPUT:
+
+	HISTORY:
+
+	"""
 	    	r=np.sqrt(x**2+y**2+z**2)
     		pot=self.potential(x,y,z)		#eventuell weitere Parameter im Potential
 		    E=self.energy(x,y,z,vx,vy,vz)
-		    L=elf.angularmom(x,y,z,vx,vy,vz)[0] #25. Tippfehler in self
+		    L=self.angularmom(x,y,z,vx,vy,vz)[0] 
     		return (1/r_ap)**2.+2.*(self.potential(r_ap)-E)/L**2. 	#Potential im Apo- bzw Pericenter noch richtige Argumente/Parameter einsetzen
             #^-- 28. Diese Funktion sollte meiner Meinung nach rmin und rmax returnen.
 	
@@ -165,6 +305,18 @@ class GCorbit:
     #27. Ich wuerde noch eine Funktion _periapocenter_aux(E,L) definieren, und die dann in periapocenter in fsolve aufrufen.
 
 	def j_rint(self,x,y,z,vx,vy,vz):
+	"""
+	NAME:
+
+	PURPOSE:
+
+	INPUT:
+
+	OUTPUT:
+
+	HISTORY:
+
+	"""
     		r=np.sqrt(x**2+y**2+z**2)
     		pot=self.potential(x,y,z)
     		E=self.energy(x,y,z,vx,vy,vz)
@@ -173,11 +325,35 @@ class GCorbit:
     #29. j_rint ist doch die funktion die dann in J_r als integrand im Integral aufgerufen wird, oder? Das Integral ist ueber r . Das heisst, du uebergibst dieser Funktion NUR das r und ausserdem die Konstanten E und L, die du schon vorher in J_r ausgerechnet hast.
 
 	def J_phi(self,x,y,z,vx,vy,vz):
+	"""
+	NAME:
+
+	PURPOSE:
+
+	INPUT:
+
+	OUTPUT:
+
+	HISTORY:
+
+	"""
     		Lz=self.angularmom(x,y,z,vx,vy,vz)[3]
     		J_phi=Lz
     		return J_phi
     
 	def J_theta(self,x,y,z,vx,vy,vz):
+	"""
+	NAME:
+
+	PURPOSE:
+
+	INPUT:
+
+	OUTPUT:
+
+	HISTORY:
+
+	"""
     		L=self.angularmom(x,y,z,vx,vy,vz)[0]
     		Lz=self.angularmom(x,y,z,vx,vy,vz)[1]
     		J_theta=L-np.abs(Lz)
@@ -186,6 +362,18 @@ class GCorbit:
 ### J_r beim Integral unsicher wegen Argumenten f�r j_rint und wegen Apo- und Pericenter ###
 
 	def J_r(self,x,y,z,vx,vy,vz):
+	"""
+	NAME:
+
+	PURPOSE:
+
+	INPUT:
+
+	OUTPUT:
+
+	HISTORY:
+
+	"""
 
 	### Peri- und Apocenter Suche noch verbessern, nicht min(rl)/max(rl) sondern irgendwie kleine bzw gro�e Werte finden abhaengig von r ###
 		rmin=opt.fsolve(periapocenter,np.min(r)) #nicht min(rl) sondern einfach kleiner Wert weil ich es erst durch orbit integration weiss
@@ -196,6 +384,18 @@ class GCorbit:
     		return J_r
 	
 	def actions(self,x,y,z,vx,vy,vz):
+	"""
+	NAME:
+
+	PURPOSE:
+
+	INPUT:
+
+	OUTPUT:
+
+	HISTORY:
+
+	"""
 		J_phi=self.J_phi(x,y,z,vx,vy,vz)
 		J_theta=self.J_theta(x,y,z,vx,vy,vz)
 		J_r=self.J_r(x,y,z,vx,vy,vz)
